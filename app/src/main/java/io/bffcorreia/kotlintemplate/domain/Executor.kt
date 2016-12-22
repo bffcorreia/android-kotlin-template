f298a17558557192a8f3e4728b54f79b5a533a35
Bruno Correia <bffcorreia@gmail.com>
2016-12-22 00:17:58 +0000

    :lipstick:

--- app/src/main/java/io/bffcorreia/kotlintemplate/domain/Executor.kt
+++ app/src/main/java/io/bffcorreia/kotlintemplate/domain/Executor.kt
@@ -1,10 +1,8 @@
 package io.bffcorreia.kotlintemplate.domain
 
-class Executor {
-  companion object {
-    const val MAIN = "main"
-    const val NEW = "new"
-    const val IO = "io"
-  }
+object Executor {
+  const val MAIN = "main"
+  const val NEW = "new"
+  const val IO = "io"
 }
 
